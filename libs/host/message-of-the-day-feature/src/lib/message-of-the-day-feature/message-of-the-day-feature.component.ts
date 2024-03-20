25cb81407eef2bd31c59d1064a712f40b66f655d
Maeran Alexandru <alex_maie@yahoo.com>
2024-03-20 14:14:26 +0200

    Begin adding a11y to motd

--- libs/host/message-of-the-day-feature/src/lib/message-of-the-day-feature/message-of-the-day-feature.component.ts
+++ libs/host/message-of-the-day-feature/src/lib/message-of-the-day-feature/message-of-the-day-feature.component.ts
@@ -1,4 +1,9 @@
-import { ChangeDetectionStrategy, Component, inject } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  HostListener,
+  inject,
+} from '@angular/core';
 import { LetDirective } from '@ngrx/component';
 import { CommonModule } from '@angular/common';
 import { MessageOfTheDayFeatureService } from './services/message-of-the-day-feature.service';
@@ -13,6 +18,9 @@ import { Observable } from 'rxjs';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MessageOfTheDayFeatureComponent {
+  public simulateHoverEfectUpArrow: boolean = false;
+  public simulateHoverEfectDownArrow: boolean = false;
+
   private _messageOfTheDayService: MessageOfTheDayFeatureService = inject(
     MessageOfTheDayFeatureService
   );
@@ -29,6 +37,32 @@ export class MessageOfTheDayFeatureComponent {
   }
 
   public downArrowClick(): void {
+    this._messageOfTheDayService.getPreviousMessage();
+  }
+
+  @HostListener('window:keydown.ArrowUp', ['$event']) onArrowUp(
+    ev: KeyboardEvent
+  ) {
+    this._messageOfTheDayService.getNextMessage();
+    this.simulateHoverEfectUpArrow = true;
+    setTimeout(() => this.simulateHoverEfectUpArrow = false, 1000);
+  }
+
+  @HostListener('window:keydown.ArrowRight', ['$event']) onArrowRight(
+    ev: KeyboardEvent
+  ) {
+    this._messageOfTheDayService.getNextMessage();
+  }
+
+  @HostListener('window:keydown.ArrowDown', ['$event']) onArrowDown(
+    ev: KeyboardEvent
+  ) {
+    this._messageOfTheDayService.getPreviousMessage();
+  }
+
+  @HostListener('window:keydown.ArrowLeft', ['$event']) onArrowLeft(
+    ev: KeyboardEvent
+  ) {
     this._messageOfTheDayService.getPreviousMessage();
   }
 }
