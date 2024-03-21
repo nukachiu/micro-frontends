d808dac594a29f8b740887617dcbf0ae644364cb
Maeran Alexandru <alex_maie@yahoo.com>
2024-03-21 22:10:53 +0200

    Load a basic component

--- libs/widget-canvas/widget-canvas-feature/src/lib/widget-canvas-feature/widget-canvas-feature.component.ts
+++ libs/widget-canvas/widget-canvas-feature/src/lib/widget-canvas-feature/widget-canvas-feature.component.ts
@@ -1,7 +1,15 @@
-import { ChangeDetectionStrategy, Component, HostListener, Input } from '@angular/core';
+import {
+  ChangeDetectionStrategy,
+  Component,
+  ComponentFactoryResolver,
+  HostListener,
+  Input,
+  ViewChild,
+  ViewContainerRef,
+} from '@angular/core';
 import { CommonModule } from '@angular/common';
 
-type CssStyle = {[key: string]: string};
+type CssStyle = { [key: string]: string };
 
 @Component({
   selector: 'widget-canvas-feature',
@@ -18,14 +26,24 @@ export class WidgetCanvasFeatureComponent {
   @Input()
   public cssStyle: CssStyle = {};
 
-  public readonly verticallySplitTopDivStyle: CssStyle = {
+  @ViewChild('container', { read: ViewContainerRef }) container: ViewContainerRef | undefined;
+
+    
+  public backgroundColor = '';
+
+  public constructor(private readonly _resolver: ComponentFactoryResolver) {
+    this.backgroundColor= this._createRandomColor();
+  }
+
+  public readonly horizontallySplitTopDivStyle: CssStyle = {
     position: 'relative',
     height: 'calc(50% - 2px)',
+    width: '100%',
     top: '-2px',
     left: '-2px',
   };
 
-  public readonly verticallySplitDownDivStyle: CssStyle = {
+  public readonly horizontallySplitDownDivStyle: CssStyle = {
     position: 'relative',
     width: '100%',
     height: '50%',
@@ -33,15 +51,18 @@ export class WidgetCanvasFeatureComponent {
     left: '-2px',
   };
 
-  public readonly horizontallySplitDivStyle: CssStyle = {
+  public readonly verticallySplitDivStyle: CssStyle = {
     width: '100%',
     position: 'relative',
+    height: '100%',
     top: '-2px',
     left: '-2px',
   };
 
   public isSplitHorizontally: boolean = false;
   public isSplitVertically: boolean = false;
+
+  public isRemoteComponentLoaded: boolean = false;
 
   public divedeDivVertically(e: Event): void {
     e.stopPropagation();
@@ -53,9 +74,52 @@ export class WidgetCanvasFeatureComponent {
     this.isSplitHorizontally = true;
   }
 
+  public loadComponent() {
+    // const componentType = null;
+    const factory = this._resolver.resolveComponentFactory(SimpleComponentOne);
+    this.container?.clear();
+    const componentRef = this.container?.createComponent(factory);
+    this.isRemoteComponentLoaded = true;
+  }
+
   @HostListener('click', ['$event'])
   public onClick(e: Event) {
     e.stopPropagation();
-    console.log(123);
+    this.loadComponent();
+  }
+
+  private _createRandomColor(): string {
+    const r = Math.floor(Math.random() * 256); // Random number between 0 and 255 for red
+    const g = Math.floor(Math.random() * 256); // Random number between 0 and 255 for green
+    const b = Math.floor(Math.random() * 256); // Random number between 0 and 255 for blue
+
+    return `rgb(${r}, ${g}, ${b})`;
   }
 }
+
+
+@Component({
+  selector: 'app-simple-component-one',
+  template: `<p>Simple Component One works!</p>`,
+  styles: [
+    `
+      p {
+        color: blue;
+      }
+    `,
+  ],
+})
+export class SimpleComponentOne {}
+
+@Component({
+  selector: 'app-simple-component-two',
+  template: `<p>Simple Component Two works!</p>`,
+  styles: [
+    `
+      p {
+        color: green;
+      }
+    `,
+  ],
+})
+export class SimpleComponentTwo {}
