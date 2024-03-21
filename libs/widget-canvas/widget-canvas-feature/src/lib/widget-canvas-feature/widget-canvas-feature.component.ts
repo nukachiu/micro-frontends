d79511bed1c8b668948bd19fc9842f671752cd8b
Maeran Alexandru <alex_maie@yahoo.com>
2024-03-21 14:59:13 +0200

    Implement a basic vertical split

--- libs/widget-canvas/widget-canvas-feature/src/lib/widget-canvas-feature/widget-canvas-feature.component.ts
+++ libs/widget-canvas/widget-canvas-feature/src/lib/widget-canvas-feature/widget-canvas-feature.component.ts
@@ -1,9 +1,11 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DivideDivDirective } from './directives/divide-div.directive';
 
+type CssStyle = {[key: string]: string};
+
 @Component({
-  selector: 'my-micro-frontends-widget-canvas-feature',
+  selector: 'widget-canvas-feature',
   standalone: true,
   imports: [CommonModule, DivideDivDirective],
   templateUrl: './widget-canvas-feature.component.html',
@@ -11,11 +13,30 @@ import { DivideDivDirective } from './directives/divide-div.directive';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class WidgetCanvasFeatureComponent {
+  @Input()
+  public isRoot: boolean = true;
+
+  @Input()
+  public cssStyle: CssStyle = {};
+
+  public readonly verticallySplitUpperDivStyle: CssStyle = {
+    position: 'absolute',
+    height: 'calc(50% - 2px)',
+    top: '-2px',
+    left: '-2px',
+  };
+
+  public readonly horizontallySplitUpperDivStyle: CssStyle = {
+    position: 'absolute',
+    height: '50%',
+    top: 'calc(50% - 2px)',
+    left: '-2px',
+  };
+
   public isSplitHorizontally: boolean = false;
   public isSplitVertically: boolean = false;
 
   public divedeDivVertically(): void {
-    alert('divided vertically');
     this.isSplitVertically = true;
   }
 
